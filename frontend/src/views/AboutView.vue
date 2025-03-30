8339094a91cc06ab6083eba88758037ee77fb596
wea_ondara <wea_ondara@wea-ondara.net>
2025-03-30 19:16:38 +0200

    populate about page

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,15 +1,18 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="p-3 overflow-y-auto">
+    <div class="mb-3">
+      <h1>About</h1>
+      This is small personal project to track Pokémon card collections.
+    </div>
+    <div>
+      <h2>API credits</h2>
+      <div>
+        <img src="https://tcgdex.dev/favicon.ico" alt="TCGdex logo" height="16" width="16" class="me-1">
+        <a href="https://tcgdex.dev/">TCGdex</a> (card and set data)
+      </div>
+    </div>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<script setup lang="ts">
+</script>
