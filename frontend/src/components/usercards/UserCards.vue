ff43b84352741a38d4254951a50dd80d76d5ecd6
wea_ondara <wea_ondara@wea-ondara.net>
2025-05-15 21:56:40 +0200

    added search to user cards

--- frontend/src/components/usercards/UserCards.vue
+++ frontend/src/components/usercards/UserCards.vue
@@ -8,8 +8,11 @@ import '@/assets/vue-good-table/mobile.scss';
 import Loading from '@/components/Loading.vue';
 import CardBsCard from "@/components/cards/CardBsCard.vue";
 import {CardsStore} from "@/stores/CardsStore";
-import {CardBriefVmV1, CardVmV1, UserCardVmV1} from "pokecards-oas";
+import {CardVmV1, SetVmV1, UserCardVmV1} from "pokecards-oas";
 import UserCardEditModal from "@/components/cards/UserCardEditModal.vue";
+import {SetsStore} from '@/stores/SetsStore.ts';
+
+type CardFilterPredicate = (card: CardVmV1) => boolean;
 
 @Component({
   components: {
@@ -22,9 +25,13 @@ import UserCardEditModal from "@/components/cards/UserCardEditModal.vue";
 export default class UserCards extends Vue {
   readonly userCardsStore = new UserCardsStore();
   readonly cardStore = new CardsStore();
+  readonly setsStore = new SetsStore();
+
+  filter = '';
 
   async mounted(): Promise<void> {
     await this.userCardsStore.loadIfAbsent();
+    await this.setsStore.loadIfAbsent();
   }
 
   get cards(): { userCard: UserCardVmV1, card: CardVmV1 | null }[] {
@@ -34,13 +41,50 @@ export default class UserCards extends Vue {
     }));
   }
 
+  get filteredCards(): { userCard: UserCardVmV1, card: CardVmV1 | null }[] {
+    let trimmedFilter = this.filter.trim();
+    if (!trimmedFilter.length) {
+      return this.cards;
+    }
+
+    //filter
+    let terms = trimmedFilter.split(' ');
+    let numbers = new Set(terms.filter((t) => t.match(/^[0-9]+$/)).map(t => parseInt(t)));
+    let texts = terms.filter((t) => !t.match(/^[0-9]+$/));
+
+    let predicates: CardFilterPredicate[] = [];
+    if (numbers.size) {
+      predicates.push((card: CardVmV1) => numbers.has(parseInt(card.number ?? card.id.split('-')[1] ?? '-1')));
+    }
+
+    if (texts.length) {
+      const lowerTexts = new Set(texts.map(e => e.toLocaleLowerCase()));
+      let matchingSets = this.setsStore.sets.filter((s: SetVmV1) => lowerTexts.has(s.abbreviation.toLocaleLowerCase()));
+      if (matchingSets.length) {
+        const setPredicates: CardFilterPredicate[]
+            = matchingSets.map((s: SetVmV1) => ((card: CardVmV1) => card.setId === s.id && card.language === s.language));
+        predicates.push((card: CardVmV1) => setPredicates.some(p => p(card)));
+      }
+
+      const allSetAbbreviations = this.setsStore.sets.map((s: SetVmV1) => s.abbreviation.toLocaleLowerCase());
+      const remainingTexts = texts.map(t => t.toLocaleLowerCase()).filter(t => !allSetAbbreviations.includes(t));
+
+      if (remainingTexts.length) {
+        predicates.push((card: CardVmV1) => remainingTexts.some(t => card.name.toLocaleLowerCase().includes(t.toLocaleLowerCase())));
+      }
+    }
+    console.log(predicates);
+
+    return this.cards.filter(e => e.card && predicates.every(p => p(e.card!)));
+  }
+
   getOrFetchCard(uid: string): CardVmV1 | null {
     const card = this.cardStore.cardsByUid.get(uid);
     if (card) {
       return card;
     }
 
-    this.cardStore.reloadCardByUid(uid)
+    this.cardStore.reloadCardByUid(uid);
     return null;
   }
 
@@ -53,7 +97,10 @@ export default class UserCards extends Vue {
 <template>
   <div class="d-flex flex-column">
     <div class="d-flex flex-row mb-2">
-      <div class="btn-group ms-auto">
+      <div class="flex-grow-1">
+        <input class="form-control" v-model="filter"/>
+      </div>
+      <div class="btn-group ms-2">
         <button class="btn btn-secondary" @click="userCardsStore.reload()">
           <i class="fa fa-refresh"/>
         </button>
@@ -62,7 +109,7 @@ export default class UserCards extends Vue {
 
     <Loading v-if="userCardsStore.loading"/>
     <div v-else class="flex-grow-1 d-flex flex-row flex-wrap overflow-auto">
-      <div v-for="{userCard, card} in cards"
+      <div v-for="{userCard, card} in filteredCards"
            :key="userCard.id"
            class="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-12 pe-1 pb-1">
         <CardBsCard :card="card" class="c-pointer" @click="card && openCard(card, userCard)"/>
