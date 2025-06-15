8a264616ed40fc1b582b5eeef64f8101b21aca54
wea_ondara <wea_ondara@wea-ondara.net>
2025-06-15 23:11:49 +0200

    added back fallbacks for card and price loading in case the single request loading fails

--- frontend/src/util/preload.ts
+++ frontend/src/util/preload.ts
@@ -19,6 +19,19 @@ export async function preload(): Promise<void> {
   const cardUids = [...new Set(userCardsStore.userCards.map((e: UserCardVmV1) => e.cardUid))] as string[];
   await cardStore.reloadCardsByUids(cardUids);
 
+  //cards fallback and missing cards
+  const cardFutures = cardUids.map((uid: string) => {
+    let future = Promise.resolve();
+
+    const card = cardStore.cardsByUid.get(uid);
+    if (!card) {
+      future = future.then(() => cardStore.reloadCardByUid(uid));
+    }
+
+    return future;
+  });
+  await Promise.allSettled(cardFutures);
+
   //prices
   const cardIds = [...new Set(cardUids
     .map((uid: string) => cardStore.cardsByUid.get(uid)?.id)
@@ -26,4 +39,17 @@ export async function preload(): Promise<void> {
     .map(e => e!),
   )];
   await priceStore.reloadCardPricesByIds(cardIds);
+
+  //prices fallback and missing prices
+  const priceFutures = cardIds.map((cardId: string) => {
+    let future = Promise.resolve();
+
+    const price = priceStore.cardPricesById.get(cardId);
+    if (!price) {
+      future = future.then(() => priceStore.reloadCardPriceById(cardId));
+    }
+
+    return future;
+  });
+  await Promise.allSettled(priceFutures);
 }
