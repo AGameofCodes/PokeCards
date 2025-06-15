48efdb70f1f2b370e1d5b3f0fdd923918e9b54e8
wea_ondara <wea_ondara@wea-ondara.net>
2025-06-15 23:04:18 +0200

    increase max http header size for requests with super long get params

--- build.sh
+++ build.sh
@@ -24,5 +24,5 @@ cp -r frontend/dist/* dist/_client
 # create start script
 echo '#!/bin/bash' > dist/start.sh
 echo 'cd "$(dirname "$0")"' >> dist/start.sh
-echo 'node main.js' >> dist/start.sh
+echo 'node --max-http-header-size=65536 main.js' >> dist/start.sh
 chmod +x dist/start.sh
